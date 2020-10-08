1dfb73ef633c3c8e88ccb4bd9c76edba52df4674
Kento Nakano <hakuyowasedatech@gmail.com>
2020-10-08 15:12:46 +0900

    buildをdocsに変更

---
+++ docs/precache-manifest.a63052b8ba9146352b42a321b898e298.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8983fe1ecb3f117ebd17f4a1a1aac4",
+    "url": "/0716_test/index.html"
+  },
+  {
+    "revision": "9934150f844d0db2ba39",
+    "url": "/0716_test/static/css/2.19603c2e.chunk.css"
+  },
+  {
+    "revision": "5e08dc068ac9994b9545",
+    "url": "/0716_test/static/css/main.f15eef53.chunk.css"
+  },
+  {
+    "revision": "9934150f844d0db2ba39",
+    "url": "/0716_test/static/js/2.5970f76e.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/0716_test/static/js/2.5970f76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e08dc068ac9994b9545",
+    "url": "/0716_test/static/js/main.8f66a22f.chunk.js"
+  },
+  {
+    "revision": "9002aa15e01df5556236",
+    "url": "/0716_test/static/js/runtime-main.8098606a.js"
+  },
+  {
+    "revision": "2950cd1279dcfd000ffd5bc83de54551",
+    "url": "/0716_test/static/media/01.2950cd12.jpg"
+  },
+  {
+    "revision": "23a6ae9429176903547b08adf7d864ee",
+    "url": "/0716_test/static/media/02.23a6ae94.jpg"
+  },
+  {
+    "revision": "bd898375b7c427b5f60ec511752620ee",
+    "url": "/0716_test/static/media/03.bd898375.jpg"
+  },
+  {
+    "revision": "f797e29440c5b9418ba8d96274d3694e",
+    "url": "/0716_test/static/media/04.f797e294.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/0716_test/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/0716_test/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/0716_test/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/0716_test/static/media/slick.f97e3bbf.svg"
+  }
+]);
